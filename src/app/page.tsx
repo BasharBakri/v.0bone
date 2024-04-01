7bd600a43d114436b330aa1c7bf362b09467be2f
BasharBakri <basharbakriwork@gmail.com>
2024-04-01 15:32:56 +0300

    base side-nav layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import SideNav from "./ui/sidenav";
 export default function Home() {
   return (
     <main className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-      <aside className="w-full flex-none md:w-64 bg-red-300 border border-purple-500">
+      <aside className=" flex-none">
         <SideNav />
       </aside>
       <div className="flex-grow p-6 md:overflow-y-auto md:p-12 bg-red-500">hello</div>
--- src/app/ui/sidenav.tsx
+++ src/app/ui/sidenav.tsx
@@ -1,15 +1,22 @@
 import Link from 'next/link';
+import Image from 'next/image';
+import imageSource from '../../../public/data-span-logo.png'
 
 export default function SideNav() {
   return (
-    <nav className=" h-full flex-col px-3 py-4  border-green-500 border-solid border bg-yellow-950">
+    <nav className=" h-full flex-col px-8 py-4  border-green-500 border-solid border bg-white">
+
       <div
-        className="mb-2 flex h-full rounded-md bg-blue-600 p-4 "
-      > logo
-        <div className="w-32 text-white md:w-40">
-          text 2
-        </div>
+        className="mb-2 h-full border border-solid border-gray-300 rounded-lg p-4 "
+      >
+        <Image src={imageSource}
+          width={242}
+          height={65}
+          alt="Screenshots of the dashboard project showing desktop version"
+        />
+        <p className="font-semibold text-gray-800 mt-12">Classes filters</p>
       </div>
     </nav>
   );
 }
+
